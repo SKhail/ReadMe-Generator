d9587520731cbbf5597744bbb4ab0eadc06edf51
Said Khail <saidkhail@hotmail.com>
2024-02-19 15:30:13 +0000

    Added the html file to be displayed as a README file

--- index.js
+++ index.js
@@ -1,61 +1,81 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [
- {
-  type: 'input',
-  name: 'title',
-  message: 'What is the title of your project?'
- },
- {
-  type: 'input',
-  name: 'description',
-  message: 'Enter the description'
- },
- {
-  type: 'input',
-  name: 'Installation',
-  message: 'Provide step by step guide to install your poject',
- },
- {
-  type: 'input',
-  name: 'Usage',
-  message: 'Provide the use of your project',
- },
- {
-  type: 'input',
-  name: 'Installation',
-  message: 'Provide step by step guide to install your project',
- },
- {
-  type: 'list',
-  name: 'license',
-  message: 'What type of license does your project need?',
-  choices: ['None', 'MIT', 'Apache 2.0', 'GPL v3', 'BSD 3']
- },
- {
-  type: 'input',
-  name: 'Contribution',
-  message: 'What guidelines must others follow when contributing to the project?',
- },
- {
-  type: 'input',
-  name: 'test',
-  message: 'How do you test this project?',
- },
- {
-  type: 'input',
-  name: 'askGithub',
-  message: 'If someone wants to contact you what is your Github username?',
- },
- {
-  type: 'input',
-  name: 'askEmail',
-  message: 'What is your Email Address?',
- },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: function (value) {
+      if (value.trim() === '') {
+        return 'Please enter a title for your project '
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: ' Enter the description of your project?',
+    validate: function (value) {
+      if (value.trim() === '') {
+        return 'Please enter the description of your project? '
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: ' Provide the usage of your project',
+    validate: function (value) {
+      if (value.trim() === '') {
+        return 'Please add information about the usage of your project '
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide a step by step guide on how to install your project',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What type of license does your project require?',
+    choices: ['None', 'MIT', 'Apache 2.0', 'GPL v3', 'BSD 3']
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What guidelines must others follow when contributing to the project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How do you test this project?',
+  },
+  {
+    type: 'input',
+    name: 'askGithub',
+    message: 'If someone wants to contact you what is your Github username?',
+  },
+  {
+    type: 'input',
+    name: 'askEmail',
+    message: 'What is your Email Address?',
+    validate: function (value) {
+      const emailChar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  //Check if it meets the correct email address format
+      if (!emailChar.test(value)) {
+        return 'Enter a valid Email Address'
+      }
+      return true;
+    }
+  },
 
 ];
 
@@ -64,32 +84,32 @@ const questions = [
 
 // }
 
-const writeToFile = fileInfo => {
- return new Promise((resolve, reject) => {
-  fs.writeFile('./generateREADME.md', fileInfo, err => {
-   if (err) {
-    reject(err);
-    return;
-   }
-   resolve({
-    ok: true
-   });
+const writeToFile = markdownContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./generateREADME.md', markdownContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true
+      });
+    });
   });
- });
 }
 
 // function to initialize program
 function init() {
- inquirer.prompt(questions)
-  .then(function (answer) {
-   console.log(answer);
-   const fileInfo = generateMarkdown(answer)
-   writeToFile(fileInfo)
-  });
+  inquirer.prompt(questions)
+    .then(function (data) {
+      console.log(data);
+      const markdownContent = generateMarkdown(data)
+      writeToFile(markdownContent)
+    });
 }
 
 // function call to initialize program
 init();
 
 //Exporting the questions
-module.export = questions;
+module.exports = questions;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,96 @@
 // required packages
-const fs = require("fs");
-
-const inquirer = require("inquirer");
-const indexFile = require("./index.js");
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const indexFile = require("../index.js");
+// const boxen = require('boxen');
+// const chalk = require('chalk');
+// import boxen from 'boxen';
+// import chalk from 'chalk';
 
 //function for generating licenese badges 
-function generateLicenseBadge() {
-  let itembadge = '';
-  //changing the if/else to a ternary operator
-  itembadge = license === 'MIT' ? '[![ MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]' :
-    license === 'Apache 2.0' ? '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)]' :
-      license === 'GPL v3' ? '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)]' :
-        license === 'BSD 3' ? '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)]' :
-          badge;
+function generateLicenseBadge(license) {
+  let itembadge = ';'
 
-  // if (license === 'MIT') {
-  //   itembadge = `[![ MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
-  // } else if(license === 'Apache 2.0') {
-  //   itembadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-  // } else if (license === 'GPL v3') {
-  //   `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-  // } else if (license === 'BSD 3'){
-  //   `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
-  // } else {
-  //   return badge;
-  // }
-  // }
+  //changing the if/else to a ternary operator
+  itembadge = license === 'MIT' ? '[![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]' :
+    license === 'Apache 2.0' ? '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' :
+      license === 'GPL v3' ? '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)' :
+        license === 'BSD 3' ? '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)' :
+          '';
+  ;
+
+  return itembadge;
+
 }
 
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
+  // return 
+  return `
+    # ${data.title}
+      
+    ## Table of Content
+    ###[description](#description)
+    ### [usage] (#usage)
+    ### [installation] (#installation)
+    ### [license] (#license)
+    ### [contribution] (#contribution)
+    ### [tests] (#test)
+
+    ## Description
+    <a name="description"></a>${data.description}
+
+    ## Usage
+    <a name="usage"></a> ${data.usage}
+
+
+    ## Installation
+    <a name="installation"></a>${data.installation}
+
+    ## License
+    <a name="license"></a>${generateLicenseBadge(data.license)}
+
+    ## Contribution
+    <a name="contribution"></a>${data.contribution}
+
+    ## Tests
+    <a name="tests"></a>${data.tests}
+
+    ## Github
+
+    ### Github Profile is: https://github.com/${data.askGithub}
+
+    ## Email;
+    ### If you would like to give your email address: ${data.askEmail}
+
 `;
+  // //use boxen to change the readme file look
+  // const boxedContent = boxen(content, { padding: 1 });
+  // // console.log(boxedContent);
+
+  // //use chalk to add colour to it 
+  // const colouredContent = chalk.backgroundColorNames(boxedContent);
+  // // console.log(colouredContent);
+
+  // return colouredContent;
 }
 
-
-
-
-
-
-
-
 module.exports = generateMarkdown;
+
+
+
+
+// not needed 
+
+// if (license === 'MIT') {
+//   itembadge = `[![ MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+// } else if(license === 'Apache 2.0') {
+//   itembadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+// } else if (license === 'GPL v3') {
+//   `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+// } else if (license === 'BSD 3'){
+//   `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+// } else {
+//   return badge;
+// }
+// }
